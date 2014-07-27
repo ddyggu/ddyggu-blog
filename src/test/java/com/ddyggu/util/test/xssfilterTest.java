d7592d48df1422129251595c056f414884f7be31
ddyggu <ddyggu@naver.com>
2014-07-28 00:49:49 +0900

    xss filter 세부 규칙추가

---
+++ src/test/java/com/ddyggu/util/test/xssfilterTest.java
@@ -0,0 +1,29 @@
+package com.ddyggu.util.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.nhncorp.lucy.security.xss.XssFilter;
+
+public class xssfilterTest {
+
+	@Test
+	public void test() throws Exception {
+		XssFilter filter = XssFilter.getInstance("lucy-xss-superset.xml");
+		String dirty = "<span style='font-size: 9pt;'><p><br></p>" +
+							"<p><br></p><span style='font-family: Gulim,굴림,AppleGothic,sans-serif; font-size: 11pt;'><p><br></p>" +
+							"<p><br></p><span style='font-family: Gulim,굴림,AppleGothic,sans-serif; font-size: 9pt;'><p><br></p><strong><p><br></p>" +
+							"<p><br></p></strong></span></span></span><p><br></p>";
+		String clean = filter.doFilter(dirty);
+		System.out.println(clean);
+		
+		System.out.println("---------------------------------------------------------------");
+		
+		String dirty2 = "<span style='font-size: 9pt;'><p></span>";
+		String clean2 = filter.doFilter(dirty2);
+		System.out.println(clean2);
+		
+	}
+
+}
